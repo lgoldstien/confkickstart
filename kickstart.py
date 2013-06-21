8237475edfe8cfcef75c31e140f1abb7d6b4e325
lgoldstien <lgoldstien@gmail.com>
2013-06-21 11:23:21 +0200

    Add stdout colour function

--- kickstart.py
+++ kickstart.py
@@ -14,11 +14,16 @@ import os
 def getSciptOptions():
     return os.walk('./starters/').next()[1]
 
+def strColour(string, colour):
+    colours = { 'red': "\033[91m", 'green': "\033[32m", 'blue': "\033[34m" }
+    stop = "\033[00m"
+    return colours[colour] + string + stop
+
 def main():
     if len(sys.argv) > 1 :
         application = sys.argv[1]
     else :
-        print "No application was selected!"
+        print strColour("No application was selected!", "red")
         print "Please choose one of the following:"
         print ""
         for app in getSciptOptions():
